e9c3e32e85ad05f2eb30e38c5ba7014ce8377581
=Jaden Leng <=s10258718@connect.np.edu.sg>
2025-05-05 15:02:38 +0800

    Updated index.php to trigger pipeline

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 // Sample PHP file
-$name = "Mr Jaden Leng -CNAS";
-$message = "Hello, $name! Welcome to  CNAS 1.";
+$name = "Mr  <own student name> - CNAS";
+$message = "Hello, $name! CNAS has been changed.";
 echo $message;
 ?>
